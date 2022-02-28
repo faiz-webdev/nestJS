feb8f6aae925c38a20eebcb1d794b7e56299b414
faijuddin <faijuddin@intelegencia.com>
2022-02-28 16:13:04 +0530

    validation error and database connection

---
+++ src/tasks/dto/get-task-filter.dto.ts
@@ -0,0 +1,11 @@
+import { IsEnum, IsOptional, IsString } from 'class-validator';
+import { TasksStatus } from '../task.model';
+export class GetTasksFilterDto {
+  @IsOptional()
+  @IsEnum(TasksStatus)
+  status?: TasksStatus;
+
+  @IsOptional()
+  @IsString()
+  search?: string;
+}
